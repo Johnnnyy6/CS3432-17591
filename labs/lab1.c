7fc9bfc4cf43a17bdd16c1cf105f8df61db77aff
Jonathan Perea <jonathan@Jonathans-MacBook-Pro.local>
2021-09-14 10:35:23 -0600

    updated lab 1 and added tokenize function

--- labs/lab1.c
+++ labs/lab1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#define MAX_LIMIT 80
+
+char** tokenize(char* str);
+
 
 int main()
 {
-    printf("hello, from lab 1\n");
-    return(0);
+   char str[MAX_LIMIT];
+   printf("Enter a sentence to be tokenized:\n");
+   fgets(str, MAX_LIMIT, stdin);
+
+   char* x;
+   tokenize(x);
+   return 0;
+}
+
+char** tokenize(char* str){
+    return 0;
 }
